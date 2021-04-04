784d85a2b4e755a4c3e400d4fbe9dc098737730a
Alexey Storozhuk <alexcandoitt@gmail.com>
2021-04-04 13:03:13 +0300

    Корректировка header и header-lib

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import './sass/main.scss';
 
-import movieCardTpl from './templates/movie-card.hbs';
-const refs = {
-  cardContainer: document.querySelector('.gallery__list'),
-};
+// import movieCardTpl from './templates/movie-card.hbs';
+// const refs = {
+//   cardContainer: document.querySelector('.gallery__list'),
+// };
 
-fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=ade6c0d166c517e1fe622025fc12c476')
-  .then(response => {
-    return response.json();
-  })
-  .then(trending => {
-    console.log(trending);
-    const markUp = movieCardTpl(trending);
-    console.log(markUp);
-    refs.cardContainer.innerHtml = markUp;
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=ade6c0d166c517e1fe622025fc12c476')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(trending => {
+//     console.log(trending);
+//     const markUp = movieCardTpl(trending);
+//     console.log(markUp);
+//     refs.cardContainer.innerHtml = markUp;
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
