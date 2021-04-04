2e320410b3e5d5955d30a3e3fad2f5a0560f3adc
Alexey Storozhuk <alexcandoitt@gmail.com>
2021-04-04 20:58:24 +0300

    Подготовлено к рефакторингу modal.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import movieApi from './js/movie.js';
 import genresApi from './js/genres.js';
 import movieCardTpl from './templates/movieCard.hbs';
 import movieAdapter from './js/adapters.js';
-import listPop from './pop';
-
-listPop();
 
 movieApi.fetchPopular().then(({ results }) => {
   const movieDataList = results.map(item => movieAdapter(item));
